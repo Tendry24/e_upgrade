9b45ae5f8d692726a5743392bf2a9c3049acb038
Tendry24 <hei.tendry.2@gmail.com>
2023-08-26 15:46:27 +0300

    feat : All User route and service added

---
+++ src/main/java/Tendry/e_upgrade/controller/UserController.java
@@ -0,0 +1,55 @@
+package Tendry.e_upgrade.controller;
+
+import Tendry.e_upgrade.models.User;
+import Tendry.e_upgrade.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+    private UserService service;
+
+    @GetMapping("/allUsers")
+    public List<User> getAllUsers(){
+        return service.findAllUser();
+    }
+
+    @GetMapping("/user/{id}")
+    public Optional<User> findUserById(@PathVariable int id){
+        return service.findUserById(id);
+    }
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user ) {
+        return service.insert(user);
+    }
+
+
+    @DeleteMapping("/deleteUser/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable int id , HttpStatus done , HttpStatus fail) {
+        Optional<User> deletedUser = service.delete(id);
+        if (deletedUser.isPresent()) {
+            return new ResponseEntity<>( done.OK);
+        } else {
+            return new ResponseEntity<>(fail.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/updateUser/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable int id, @RequestBody User updatedUser) {
+        updatedUser.setId(id);
+        User updated = service.update(updatedUser);
+
+        if (updated != null) {
+            return new ResponseEntity<>(id + " has been updated.", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(id + " not found.", HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/Tendry/e_upgrade/services/UserService.java
@@ -0,0 +1,61 @@
+package Tendry.e_upgrade.services;
+
+import Tendry.e_upgrade.models.User;
+import Tendry.e_upgrade.repository.user.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+
+public class UserService {
+    private UserRepository user;
+
+    public List<User> findAllUser(){
+        try{
+            return user.findAllUser();
+        }catch (SQLException e){
+            throw new RuntimeException("Error");
+        }
+    }
+
+    public Optional<User> findUserById(int id){
+        try{
+            return user.findUserById(id);
+        } catch (SQLException e){
+            throw new RuntimeException("Error");
+        }
+    }
+
+    public User insert(User toInsert){
+        try{
+            this.user.insert(toInsert);
+            return toInsert;
+        } catch (SQLException e){
+            throw new RuntimeException("Error");
+        }
+    }
+
+
+    public Optional<User> delete(int toDelete){
+        try {
+            this.user.delete(toDelete);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public User update(User updatedUser) {
+        try {
+            this.user.update(updatedUser);
+            return updatedUser;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error updating category");
+        }
+    }
+}
