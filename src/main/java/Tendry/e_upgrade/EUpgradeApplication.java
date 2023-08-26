1ef73d5eaa34497c88e8a2cf4ac21ebf87af0634
Tendry24 <hei.tendry.2@gmail.com>
2023-08-26 13:27:55 +0300

    feat : initialize the project

---
+++ src/main/java/Tendry/e_upgrade/EUpgradeApplication.java
@@ -0,0 +1,13 @@
+package Tendry.e_upgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EUpgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EUpgradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/Tendry/e_upgrade/EUpgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package Tendry.e_upgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EUpgradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
