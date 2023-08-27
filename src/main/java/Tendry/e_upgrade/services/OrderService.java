5f63911670fcdfeb113cb77081fd249acf22b2ea
Tendry24 <hei.tendry.2@gmail.com>
2023-08-27 23:55:55 +0300

    feat : all route and service added for Order

--- src/main/java/Tendry/e_upgrade/services/OrderService.java
+++ src/main/java/Tendry/e_upgrade/services/OrderService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -20,6 +21,42 @@ public class OrderService {
             return order.findAllOrders();
         } catch (SQLException e) {
             throw new RuntimeException("Error");
+        }
+    }
+
+    public Optional<Order> findOrderById(int id){
+        try{
+            return order.findOrderById(id);
+        } catch (SQLException e){
+            throw new RuntimeException("Error");
+        }
+    }
+
+    public Order insert(Order toInsert){
+        try{
+            this.order.insert(toInsert);
+            return toInsert;
+        } catch (SQLException e){
+            throw new RuntimeException("Error");
+        }
+    }
+
+
+    public Optional<Order> delete(int toDelete){
+        try {
+            this.order.delete(toDelete);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public Order update(Order updatedOrder) {
+        try {
+            this.order.update(updatedOrder);
+            return updatedOrder;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error updating Order");
         }
     }
 }
