db5faa42ef1997e2528dc217435acc696ed72ea8
Tendry24 <hei.tendry.2@gmail.com>
2023-08-27 23:55:55 +0300

    feat : migrationSQL added

--- src/main/java/Tendry/e_upgrade/EUpgradeApplication.java
+++ src/main/java/Tendry/e_upgrade/EUpgradeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EUpgradeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EUpgradeApplication.class, args);
     }
-
 }
