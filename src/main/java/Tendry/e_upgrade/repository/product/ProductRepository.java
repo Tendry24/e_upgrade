fac0ba1543314cd1e4d37bbfe70cae61260d245b
Tendry24 <hei.tendry.2@gmail.com>
2023-08-27 23:55:55 +0300

    feat : all query,route and service added for Product

--- src/main/java/Tendry/e_upgrade/repository/product/ProductRepository.java
+++ src/main/java/Tendry/e_upgrade/repository/product/ProductRepository.java
@@ -4,10 +4,7 @@ package Tendry.e_upgrade.repository.product;
 import Tendry.e_upgrade.models.Product;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +17,18 @@ public class ProductRepository extends GenericDAO {
 
     @Override
     public void insert(Product toInsert) throws SQLException {
+        String sql = "INSERT INTO product(name,description,categories_id,price,stock_quantity)";
+        try(
+                PreparedStatement statement = getConnection().prepareStatement(sql)
+        ){
+            statement.setString(1,toInsert.getName());
+            statement.setString(2,toInsert.getDescription());
+            statement.setInt(3,toInsert.getCategories_id());
+            statement.setDouble(4,toInsert.getPrice());
+            statement.setInt(5,toInsert.getStock_quantity());
 
+            statement.executeUpdate();
+        }
     }
 
     @Override
@@ -39,12 +47,47 @@ public class ProductRepository extends GenericDAO {
 
     @Override
     public Optional<Product> findProductById(int id) throws SQLException {
-        return Optional.empty();
+        String sql = "SELECT * FROM product WHERE id = ?";
+        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    return Optional.of(extractTodoFromResultSet(resultSet));
+                }
+            }
+            return Optional.empty();
+        }
     }
 
     @Override
     public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM product WHERE id = ?";
+        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+            statement.setInt(1, id);
+            int rowsAffected = statement.executeUpdate();
 
+            if (rowsAffected > 0) {
+                System.out.println( id + " has been deleted.");
+            } else {
+                System.out.println(  id + "not found.");
+            }
+        }
+    }
+
+
+    public void update(Product updatedProduct) throws SQLException {
+        String sql = "UPDATE product SET name=?,description=?,categories_id=?,price=?,stock_quantity=? WHERE id = ?";
+
+        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+            statement.setInt(1, updatedProduct.getId());
+            statement.setString(2, updatedProduct.getName());
+            statement.setString(3,updatedProduct.getDescription());
+            statement.setInt(4,updatedProduct.getCategories_id());
+            statement.setDouble(5,updatedProduct.getPrice());
+            statement.setInt(6,updatedProduct.getCategories_id());
+
+            statement.executeUpdate();
+        }
     }
 
     private Product extractTodoFromResultSet(ResultSet resultSet) throws SQLException {
